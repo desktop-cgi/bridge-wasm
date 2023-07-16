502ca41f4b5f37a19ae4601f93a3932c51faec0e
Krishnamurthy G B <3424344+ganeshkbhat@users.noreply.github.com>
2023-07-16 15:03:41 +0000

    Adding wasm language based interpretor code runs for different designs

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # bridge-wasm
 `wasm bridges` for language interpretors like `php`, `perl`, `python`,`micropython`, `cpython`, `ruby`, `julia`, `cpp`, `golang`, `ocaml`, `lua`, `napi`, `wasm-run`, `r-lang`, `openssl`, important c libraries, and other important libraries
 
-[bridge-wasm](./docs/bridge-wasm.jpeg)
+[bridge-wasm](./docs/Bridge-Wasm.jpeg)
 
 Credit: [many wasm packages taken from @polylang.io](https://github.com/chris-koch-penn/polylang.io/tree/master/wasm)
 
--- index.js
+++ index.js
@@ -4,5 +4,22 @@ function InterfaceWASM() {
 
 }
 
-var phpwasm = require("php-wasm");
+var phpwasm = require("./node_modules/php-wasm/php-node");
+// var php = new phpwasm(`<? echo "test"`);
+// php.run(`<? echo "test"`);
+
+// // // Assume add.wasm file exists that contains a single function adding 2 provided arguments
+// const fs = require('fs');
+// const path = require('path');
+
+// const wasmBuffer = fs.readFileSync(path.join(__dirname, './node_modules/php-wasm/php-node.wasm'));
+// // console.log(wasmBuffer);
+
+// WebAssembly.instantiate(wasmBuffer).then(wasmModule => {
+//     // Exported function live under instance.exports
+//     const php = wasmModule.instance.exports;
+//     //   const sum = add(5, 6);
+//     //   console.log(php); // Outputs: 11
+//     php.run();
+// });
 
